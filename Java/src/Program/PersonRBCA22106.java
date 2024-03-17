f7c838ba645779c756164eb7d3c5166749ac74bb
Ahana Koulegeri <Ahana Koulegeri@LAPTOP-CK29C7IR>
2024-03-17 18:01:17 +0530

    Commit

---
+++ Java/src/Program/PersonRBCA22106.java
@@ -0,0 +1,21 @@
+package Program;
+
+public class PersonRBCA22106 {
+	    private String name;
+	    private int age;
+
+	    public PersonRBCA22106 (String name, int age) {
+	        this.name = name;
+	        this.age = age;
+	    }
+
+	    public String getName() {
+	        return name;
+	    }
+
+	    public int getAge() {
+	        return age;
+	    }
+	}
+
+
