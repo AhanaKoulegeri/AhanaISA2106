15586de123d6854b986b048fa297742df9582a3a
Ahana Koulegeri <Ahana Koulegeri@LAPTOP-CK29C7IR>
2024-03-18 15:32:36 +0530

    Commit

---
+++ Java/src/Program/VehicleRBCA22106.java
@@ -0,0 +1,20 @@
+package Program;
+
+public class VehicleRBCA22106 {
+	    private int horsepower;
+
+	    public VehicleRBCA22106 (int horsepower) {
+	        this.horsepower = horsepower;
+	    }
+
+	    public void drive() {
+	        System.out.println("Vehicle is being driven");
+	    }
+
+	    public int getHorsepower() {
+	        return horsepower;
+	    }
+	}
+
+
+
